897240ccae540bf62ff0d383a8d57eb39755d7ac
Manas Baranwal <118442164+manasbaranwal1403@users.noreply.github.com>
2025-03-07 11:01:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function($) {
+
+	'use strict';
+
+	/************** Mixitup (Filter Projects) *********************/
+    	$('.projects-holder').mixitup({
+            effects: ['fade','grayscale'],
+            easing: 'snap',
+            transitionSpeed: 400
+        });
+
+});
+
